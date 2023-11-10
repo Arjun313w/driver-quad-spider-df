935f1129f9418ba728f869fdf9f8733bb0a719e5
Arjun313w <135212653+Arjun313w@users.noreply.github.com>
2023-11-10 15:23:51 +0530

    Restore '6/5/2023, 8:04:09 PM Initial files for MakeCode project'

--- main.ts
+++ main.ts
@@ -3,101 +3,37 @@ function stand () {
     motor.servo(motor.Servos.S8, 180)
     motor.servo(motor.Servos.S6, 0)
     motor.servo(motor.Servos.S2, 0)
+    basic.pause(500)
 }
-function BR_DFLT () {
-    motor.servo(motor.Servos.S3, 65)
-}
-function FR_DFLT () {
-    // FrontRight
-    motor.servo(motor.Servos.S4, 135)
-}
-function BL_DFLT () {
-    motor.servo(motor.Servos.S7, 45)
-}
-function BL_DOWN () {
-    motor.servo(motor.Servos.S8, 135)
-}
-function FR_body () {
-    // FrontRight
-    motor.servo(motor.Servos.S4, 90)
-}
-function all_leg_stand () {
-    motor.servo(motor.Servos.S1, 180)
-    motor.servo(motor.Servos.S2, 0)
+function walk_foreward () {
+    motor.servo(motor.Servos.S1, 140)
+    basic.pause(200)
+    motor.servo(motor.Servos.S2, 40)
+    basic.pause(200)
     motor.servo(motor.Servos.S6, 0)
-    motor.servo(motor.Servos.S8, 180)
-}
-function BR_body () {
-    // FrontRight
-    motor.servo(motor.Servos.S3, 180)
-}
-function FR_DOWN () {
+    basic.pause(200)
+    motor.servo(motor.Servos.S8, 150)
+    basic.pause(200)
     motor.servo(motor.Servos.S1, 180)
-}
-function BR_STRECH () {
-    // FrontRight
-    motor.servo(motor.Servos.S3, 0)
-}
-function FL_DOWN () {
-    motor.servo(motor.Servos.S2, 45)
-}
-function FR_STRECH () {
-    // FrontRight
-    motor.servo(motor.Servos.S4, 0)
-}
-function BL_STRECH () {
-    // FrontRight
-    motor.servo(motor.Servos.S7, 90)
-}
-function BR_DOWN () {
-    motor.servo(motor.Servos.S8, 135)
-}
-function all_leg_ground () {
-    motor.servo(motor.Servos.S1, 90)
-    motor.servo(motor.Servos.S2, 90)
-    motor.servo(motor.Servos.S6, 90)
-    motor.servo(motor.Servos.S8, 90)
-}
-function FL_STRECH () {
-    // FrontRight
-    motor.servo(motor.Servos.S5, 0)
-}
-function FR_UP () {
-    motor.servo(motor.Servos.S1, 135)
-}
-function FL_DFLT () {
-    // frontleft
-    motor.servo(motor.Servos.S5, 45)
-}
-function body () {
-    motor.servo(motor.Servos.S3, 180)
-    motor.servo(motor.Servos.S4, 90)
-    motor.servo(motor.Servos.S5, 90)
-    motor.servo(motor.Servos.S7, 0)
-}
-function BR_UP () {
-    motor.servo(motor.Servos.S8, 180)
-}
-function BL_body () {
-    // FrontRight
-    motor.servo(motor.Servos.S7, 0)
-}
-function strech () {
-    motor.servo(motor.Servos.S3, 0)
-    motor.servo(motor.Servos.S4, 180)
-    motor.servo(motor.Servos.S5, 0)
-    motor.servo(motor.Servos.S7, 90)
-}
-function FL_body () {
-    // FrontRight
-    motor.servo(motor.Servos.S5, 90)
-}
-function BL_UP () {
-    motor.servo(motor.Servos.S8, 180)
-}
-function FL_UP () {
+    basic.pause(200)
     motor.servo(motor.Servos.S2, 0)
+    basic.pause(200)
+    motor.servo(motor.Servos.S6, 40)
+    basic.pause(200)
+    motor.servo(motor.Servos.S8, 140)
+}
+function sleep () {
+    motor.servo(motor.Servos.S1, 90)
+    motor.servo(motor.Servos.S8, 90)
+    motor.servo(motor.Servos.S6, 90)
+    motor.servo(motor.Servos.S2, 90)
+    basic.pause(500)
 }
 basic.forever(function () {
-	
+    sleep()
+    basic.pause(500)
+    stand()
+    basic.pause(500)
+    walk_foreward()
+    basic.pause(2000)
 })
